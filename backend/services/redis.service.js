65ac9eb167b57c1527c3c015d868d1e93677ab5a
Tarun kumar <tk168814@gmail.com>
2025-11-07 18:49:54 +0530

    updated env file

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -4,6 +4,7 @@ const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_PASSWORD,
+  tls: {} 
 });
 
 redisClient.on('connect', () => {
